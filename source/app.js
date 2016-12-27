f74f5fb617012942695cc1e78f0e64d9cf6d570b
Adam Borowski <adam@adamboro.com>
2016-12-27 12:55:58 +0100

    use BigCalendar in app.js

--- source/app.js
+++ source/app.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
+import moment from 'moment'
+
+import BigCalendar from 'react-big-calendar'
+// a localizer for BigCalendar
+BigCalendar.momentLocalizer(moment)
+
+// this weird syntax is just a shorthand way of specifying loaders
+require('style!css!react-big-calendar/lib/css/react-big-calendar.css')
 
 class App extends React.Component {
   constructor () {
     super()
-    this.state = {
-      who: "React"
-    }
   }
   render () {
     return (
-      <div>
-        {`Hello ${this.state.who}`}
-      </div>
+      // React Components in JSX look like HTML tags
+      <BigCalendar
+        style={{height: '420px'}}
+        events={[]}
+      />
     )
   }
 }
