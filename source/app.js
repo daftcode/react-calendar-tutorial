7da5a705a2627958f4dca648f6e176eb299b6f0d
Adam Borowski <adam@adamboro.com>
2016-12-27 12:24:49 +0100

    add a basic react component

--- source/app.js
+++ source/app.js
@@ -1,2 +1,20 @@
-const who = 'World'
-console.log(`Hello ${who}`)
+import React from 'react'
+import { render } from 'react-dom'
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      who: "React"
+    }
+  }
+  render () {
+    return (
+      <div>
+        {`Hello ${this.state.who}`}
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'))
